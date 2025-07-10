2e4436a22c6442bd37b86bc95319d5d90bf68692
goelrahil <goelrahil@gmail.com>
2025-07-09 18:08:43 -0700

    sample newsletter

--- newsletter-service/admin.js
+++ newsletter-service/admin.js
@@ -1,15 +1,34 @@
-// admin.js
-import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
+// // admin.js
+// import admin from 'firebase-admin';
+// import { readFileSync } from 'fs';
 
-// Load your service account key file
+// // Load your service account key file
+// const serviceAccount = JSON.parse(
+//   readFileSync('./serviceAccountKey.json', 'utf8')
+// );
+
+// // Initialize admin SDK
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export const authAdmin = admin.auth();
+
+import admin from 'firebase-admin';
+
+const base64Config = process.env.FIREBASE_CONFIG_BASE64;
+
+if (!base64Config) {
+  throw new Error('Missing FIREBASE_CONFIG_BASE64 env variable');
+}
+
 const serviceAccount = JSON.parse(
-  readFileSync('./serviceAccountKey.json', 'utf8')
+  Buffer.from(base64Config, 'base64').toString('utf8')
 );
 
-// Initialize admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const authAdmin = admin.auth();
+export const db = admin.firestore(); // optional
